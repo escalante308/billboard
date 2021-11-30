cae579eb37aa10191f999aa19e1f9bcee46445f2
Pedro Escalante <escalante308@gmail.com>
2021-11-30 09:39:53 -0500

    First Commit

---
+++ readme.md
@@ -0,0 +1,19 @@
+# Billboard
+
+This is a VueJS application to show the Billboard Top 10 songs. It will be improved as we're getting more experienced using VueJS. 
+
+## Installation
+
+Clone the folder on your dev environment and open the ```index.html``` file
+
+## Usage
+
+Click on the different options of the app to get more info about the songs and artists.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ songs.js
@@ -0,0 +1,62 @@
+const songs = [
+    {
+        position: 1,
+        title: 'All Too Well (Taylor\'s Version)',
+        artist: 'Taylor Swift',
+        cover: 'http://i3.ytimg.com/vi/sRxrwjOtIag/hqdefault.jpg'
+    },
+    {
+        position: 2,
+        title: 'Easy On Me',
+        artist: 'Adele',
+        cover: 'http://i3.ytimg.com/vi/U3ASj1L6_sY/hqdefault.jpg'
+    },
+    {
+        position: 3,
+        title: 'Stay',
+        artist: "The Kid LAROI & Justin Bieber",
+        cover: "http://i3.ytimg.com/vi/kTJczUoc26U/hqdefault.jpg"
+    },
+    {
+        position: 4,
+        title: 'Industry Baby',
+        artist: "Lil Nas X & Jack Harlow",
+        cover: "http://i3.ytimg.com/vi/UTHLKHL_whs/hqdefault.jpg"
+    },
+    {
+        position: 5,
+        title: 'Smokin Out The Window',
+        artist: "Silk Sonic (Bruno Mars & Anderson .Paak)",
+        cover: "http://i3.ytimg.com/vi/GG7fLOmlhYg/hqdefault.jpg"
+    },
+    {
+        position: 6,
+        title: 'Bad Habits',
+        artist: "Ed Sheeran",
+        cover: "http://i3.ytimg.com/vi/orJSJGHjBLI/hqdefault.jpg"
+    },
+    {
+        position: 7,
+        title: 'Shivers',
+        artist: 'Ed Sheeran',
+        cover: "http://i3.ytimg.com/vi/Il0S8BoucSA/hqdefault.jpg"
+    },
+    {
+        position: 8,
+        title: 'Heat Waves',
+        artist: 'Glass Animals',
+        cover: "http://i3.ytimg.com/vi/mRD0-GxqHVo/hqdefault.jpg"
+    },
+    {
+        position: 9,
+        title: 'Need To Know',
+        artist: 'Doja Cat',
+        cover: "http://i3.ytimg.com/vi/dI3xkL7qUAc/hqdefault.jpg"
+    },
+    {
+        position: 10,
+        title: 'Fancy Like',
+        artist: 'Walker Hayes',
+        cover: "http://i3.ytimg.com/vi/G_zuB-ogIBw/hqdefault.jpg"
+    }
+];
