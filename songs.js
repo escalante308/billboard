095459ae5338d091a459c3c3524a18800e94103f
Pedro Escalante <escalante308@gmail.com>
2021-12-01 13:48:50 -0500

    Add Star Rating - Fix Styles

--- songs.js
+++ songs.js
@@ -3,60 +3,70 @@ const songs = [
         position: 1,
         title: 'All Too Well (Taylor\'s Version)',
         artist: 'Taylor Swift',
-        cover: 'http://i3.ytimg.com/vi/sRxrwjOtIag/hqdefault.jpg'
+        cover: 'http://i3.ytimg.com/vi/sRxrwjOtIag/hqdefault.jpg',
+        rating: "3"
     },
     {
         position: 2,
         title: 'Easy On Me',
         artist: 'Adele',
-        cover: 'http://i3.ytimg.com/vi/U3ASj1L6_sY/hqdefault.jpg'
+        cover: 'http://i3.ytimg.com/vi/U3ASj1L6_sY/hqdefault.jpg',
+        rating: "3"
     },
     {
         position: 3,
         title: 'Stay',
         artist: "The Kid LAROI & Justin Bieber",
-        cover: "http://i3.ytimg.com/vi/kTJczUoc26U/hqdefault.jpg"
+        cover: "http://i3.ytimg.com/vi/kTJczUoc26U/hqdefault.jpg",
+        rating: "3"
     },
     {
         position: 4,
         title: 'Industry Baby',
         artist: "Lil Nas X & Jack Harlow",
-        cover: "http://i3.ytimg.com/vi/UTHLKHL_whs/hqdefault.jpg"
+        cover: "http://i3.ytimg.com/vi/UTHLKHL_whs/hqdefault.jpg",
+        rating: "3"
     },
     {
         position: 5,
         title: 'Smokin Out The Window',
         artist: "Silk Sonic (Bruno Mars & Anderson .Paak)",
-        cover: "http://i3.ytimg.com/vi/GG7fLOmlhYg/hqdefault.jpg"
+        cover: "http://i3.ytimg.com/vi/GG7fLOmlhYg/hqdefault.jpg",
+        rating: "3"
     },
     {
         position: 6,
         title: 'Bad Habits',
         artist: "Ed Sheeran",
-        cover: "http://i3.ytimg.com/vi/orJSJGHjBLI/hqdefault.jpg"
+        cover: "http://i3.ytimg.com/vi/orJSJGHjBLI/hqdefault.jpg",
+        rating: "3"
     },
     {
         position: 7,
         title: 'Shivers',
         artist: 'Ed Sheeran',
-        cover: "http://i3.ytimg.com/vi/Il0S8BoucSA/hqdefault.jpg"
+        cover: "http://i3.ytimg.com/vi/Il0S8BoucSA/hqdefault.jpg",
+        rating: "3"
     },
     {
         position: 8,
         title: 'Heat Waves',
         artist: 'Glass Animals',
-        cover: "http://i3.ytimg.com/vi/mRD0-GxqHVo/hqdefault.jpg"
+        cover: "http://i3.ytimg.com/vi/mRD0-GxqHVo/hqdefault.jpg",
+        rating: "3"
     },
     {
         position: 9,
         title: 'Need To Know',
         artist: 'Doja Cat',
-        cover: "http://i3.ytimg.com/vi/dI3xkL7qUAc/hqdefault.jpg"
+        cover: "http://i3.ytimg.com/vi/dI3xkL7qUAc/hqdefault.jpg",
+        rating: "3"
     },
     {
         position: 10,
         title: 'Fancy Like',
         artist: 'Walker Hayes',
-        cover: "http://i3.ytimg.com/vi/G_zuB-ogIBw/hqdefault.jpg"
+        cover: "http://i3.ytimg.com/vi/G_zuB-ogIBw/hqdefault.jpg",
+        rating: "3"
     }
 ];
